1065fa5f64482694b2c4d185573df4308cfd9767
Philip Alekseev <phil.alekseev@gmail.com>
2017-11-10 20:44:58 -0500

    Add Google Analytics + Update map styling

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,10 +14,10 @@ $(document).ready(function () {
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
         if (re.test($(this).val())) {
-            $(".now-ui-icons.ui-1_email-85").removeClass("error-color");
+            $("span .now-ui-icons.ui-1_email-85").removeClass("error-color");
             document.getElementById("contact_submit").disabled = false;
         } else {
-            $(".now-ui-icons.ui-1_email-85").addClass("error-color");
+            $("span .now-ui-icons.ui-1_email-85").addClass("error-color");
             document.getElementById("contact_submit").disabled = true;
         }
     });
