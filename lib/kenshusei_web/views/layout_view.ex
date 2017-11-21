e6e17e7487a4146769cdb9b72029f96413780de9
Philip Alekseev <phil.alekseev@gmail.com>
2017-11-20 23:03:59 -0500

    Update meta tags and title

--- lib/kenshusei_web/views/layout_view.ex
+++ lib/kenshusei_web/views/layout_view.ex
@@ -1,3 +1,14 @@
 defmodule KenshuseiWeb.LayoutView do
   use KenshuseiWeb, :view
+  
+  def render_title([]), do: "Kenshusei Martial Arts"
+  def render_title(["about"]), do: "Kenshusei - About Us"
+  def render_title(["schedule"]), do: "Kenshusei - Our Schedule"
+  def render_title(["karate"]), do: "Kenshusei - Junior Karate"
+  def render_title(["kempo"]), do: "Kenshusei - Adult Shaolin Kempo"
+  def render_title(["jujitsu"]), do: "Kenshusei - Aiki Jujitsu"
+  def render_title(["taichi"]), do: "Kenshusei - Tai Chi Chuan"
+  def render_title(["advanced"]), do: "Kenshusei - Advanced Training"
+  def render_title([_]), do: "Kenshusei Martial Arts"
+
 end
