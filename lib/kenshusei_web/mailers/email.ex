2697264b84879ad6114aa41fdbab6f16286bee57
Philip Alekseev <phil.alekseev@gmail.com>
2018-08-16 21:35:02 -0400

    Update email templates

--- lib/kenshusei_web/mailers/email.ex
+++ lib/kenshusei_web/mailers/email.ex
@@ -6,26 +6,31 @@ defmodule Kenshusei.Email do
             to: form_submission.email,
             from: System.get_env("SENDGRID_EMAIL_RECIPIENT"),
             subject: "Kenshusei Martial Arts",
-            html_body: "<p>Hello #{form_submission.name},<p> We thank you for your interest in Kenshusei Martial Arts.<br>While we appreciate you reaching out to us via email, we are traditionally much more attainable via telephone. If you are serious about training, do not hesitate to call us at <a href='tel:9057607679'>905-760-7679</a>.</p><p>Thank you,<br>Sensei</p>",
-            text_body: "Hello #{form_submission.name}, We thank you for your interest in Kenshusei Martial Arts. While we appreciate you reaching out to us via email, we are traditionally much more attainable via telephone. If you are serious about training, do not hesitate to call us at 905-760-7679. Thank you, Sensei"
+            html_body: "<p>Hello #{form_submission.name},<p> Thank you for your interest in Kenshusei Martial Arts.<br>You should receive a response from us shortly. For more information about our training classes, please call <a href='tel:9057607679'>905-760-7679</a>.</p><p>Thank you,<br>Sensei</p>",
+            text_body: "Hello #{form_submission.name}, Thank you for your interest in Kenshusei Martial Arts. You should receive a response from us shortly. For more information about our training classes, please call 905-760-7679. Thank you, Sensei"
         )
     end
 
     def owner_email(form_submission) do
         new_email(
             to: System.get_env("SENDGRID_EMAIL_RECIPIENT"),
-            from: "noreply@kenshuseimartialarts.com",
-            subject: "Kenshusei Martial Arts",
-            html_body: "<p>Hello Sensei, <br><br>
-                        A person by the name: <strong>#{form_submission.name}</strong> <br>
-                        with the email: <strong>#{form_submission.email}</strong> <br>
-                        has recently used the contact form on your website to try and contact you.</p>
-                        <p>They have left the following message: <br><strong>#{form_submission.comment}</strong></p>",
-            text_body: "Hello Sensei, 
-                        A person by the name: #{form_submission.name} 
-                        with the email: #{form_submission.email} 
-                        has recently used the contact form on your website to try and contact you.
-                        They have left the following message: #{form_submission.comment}"
+            from: form_submission.email,
+            subject: "Kenshusei Martial Arts: Contact Form",
+            html_body:  "
+                            <p>
+                                #{form_submission.comment}
+                            </p>
+                            <p>
+                                - #{form_submission.name}
+                            </p>
+                            <br />
+                            (Email sent via contact form submission)
+                        ",
+            text_body:  "
+                            #{form_submission.comment}
+                            - #{form_submission.name}
+                            (Email sent via contact form submission)
+                        "
         )
     end
 end
